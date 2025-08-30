863986609d94794f6a83a2681f75f79dc8019b30
Alexandr <79024450444@yandex.ru>
2025-08-30 16:23:55 +0500

    Rooms table revision created

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from src.config import settings
 from src.database import Base
 from src.models.hotels import HotelsOrm
+from src.models.rooms import RoomsOrm
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
