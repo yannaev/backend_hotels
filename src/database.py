9ddb14f88d26bedf7cd9a28a7e23d7125743af70
Alexandr <79024450444@yandex.ru>
2025-08-30 14:55:50 +0500

    HotelsOrm model created

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -6,3 +7,7 @@ from src.config import settings
 engine = create_async_engine(settings.db_url)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
