0d272df83b438f20a45fba6831a58924fecced9b
Alexandr <79024450444@yandex.ru>
2025-08-31 15:27:00 +0500

    get hotels from db with filters and pagination

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -5,7 +5,7 @@ from typing import Annotated
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(default=1, ge=1, description='Номер страницы')]
-    per_page: Annotated[int | None, Query(default=5, ge=1, le=50, description='Количество на странице')]
+    per_page: Annotated[int | None, Query(None, ge=1, le=50, description='Количество на странице')]
 
 
 PaginationDep = Annotated[PaginationParams, Depends()]
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -18,11 +18,23 @@ async def get_hotels(
         id: int | None = Query(default=None, description='ID отеля'),
         title: str | None = Query(None, description='Название отеля')
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
+
+        if id:
+            query = query.filter_by(id=id)
+        if title:
+            query = query.filter_by(title=title)
+
+        query = (
+            query
+            .limit(per_page)
+            .offset(per_page * (pagination.page - 1))
+        )
         result = await session.execute(query)
-        hotels = result.scalars().all()
-        return hotels
+        hotels_ = result.scalars().all()
+        return hotels_
 
     # return hotels_[(pagination.page - 1) * pagination.per_page:pagination.page * pagination.per_page]
 
