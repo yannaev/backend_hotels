b76aafdc4c513264cdefb532c97239428d3a87f8
Alexandr <79024450444@yandex.ru>
2025-08-31 14:25:52 +0500

    Add hotels to DB in API endpoint

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,5 +1,10 @@
 from fastapi import Query, APIRouter, Body
+
+from sqlalchemy import insert
+
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix='/hotels', tags=['Отели'])
@@ -43,28 +48,27 @@ def get_hotels(
 @router.post('',
              summary='Добавить отель',
              description='Создать и добавить новый отель')
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     '1': {
         'summary': 'Marriott',
         'value': {
             'title': 'Marriott',
-            'city': 'London'
+            'location': 'London'
         }
     },
     '2': {
         'summary': 'Park INN',
         'value': {
             'title': 'Park INN',
-            'city': 'Bangkok'
+            'location': 'Bangkok'
         }
     }
 })):
-    global hotels
-    hotels.append({
-        'id': hotels[-1]['id'] + 1,
-        'title': hotel_data.title,
-        'city': hotel_data.city
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {'status': 'OK'}
 
 
--- src/schemas/hotels.py
+++ src/schemas/hotels.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, Field
 
 class Hotel(BaseModel):
     title: str
-    city: str
+    location: str
 
 class HotelPATCH(BaseModel):
     title: str | None = Field(None)
-    city: str | None = Field(None)
+    location: str | None = Field(None)
