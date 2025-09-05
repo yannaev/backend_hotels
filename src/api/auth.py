efb37492c6ea62beff186220ac507b4343e65078
Alexandr <79024450444@yandex.ru>
2025-09-06 01:07:34 +0500

    openapi examples for user registration added

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
 from passlib.context import CryptContext
 
@@ -13,7 +13,23 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post('/register',
              summary='Регистрация пользователя',
              description='Регистрация пользователя по email')
-async def register_user(data: UserRequestAdd):
+async def register_user(data: UserRequestAdd = Body(openapi_examples={
+    '1': {
+        'summary': 'User 1',
+        'value': {
+            'email': '123456@mail.ru',
+            'password': '123456'
+        },
+    },
+    '2': {
+        'summary': 'User 2',
+        'value': {
+            'email': 'alex@mail.ru',
+            'password': 'qwerty'
+        },
+    },
+
+})):
     hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
 
