0bb511a0ce9a7b3044dfec4ca385f50a8c48829d
Alexandr <79024450444@yandex.ru>
2025-09-07 02:51:02 +0500

    only_auth endpoint created

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, HTTPException, Response
+from fastapi import APIRouter, Body, HTTPException, Response, Request
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
@@ -65,3 +65,9 @@ async def login_user(
         access_token = AuthService().create_access_token({'user_id': user.id})
         response.set_cookie('access_token', access_token)
         return {'access_token': access_token}
+
+
+@router.get('/only_auth')
+async def only_auth(request: Request):
+    access_token = request.cookies.get('access_token') or None
+    return {'access_token': access_token}
