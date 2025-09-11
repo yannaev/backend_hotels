bc7da8b93de5832e15f6d8f5e138613af538f132
Alexandr <79024450444@yandex.ru>
2025-09-11 19:50:09 +0500

    dbdep added

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Body, HTTPException, Response
 
-from src.api.dependencies import UserIdDep
+from src.api.dependencies import UserIdDep, DBDep
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
@@ -12,7 +12,7 @@ router = APIRouter(prefix='/auth', tags=['Авторизация и аутент
 @router.post('/register',
              summary='Регистрация пользователя',
              description='Регистрация пользователя по email')
-async def register_user(data: UserRequestAdd = Body(openapi_examples={
+async def register_user(db: DBDep, data: UserRequestAdd = Body(openapi_examples={
     '1': {
         'summary': 'User 1',
         'value': {
@@ -31,16 +31,14 @@ async def register_user(data: UserRequestAdd = Body(openapi_examples={
 })):
     hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-
-    async with async_session_maker() as session:
-        await UsersRepository(session).add(new_user_data)
-        await session.commit()
-
+    await db.users.add(new_user_data)
+    await db.commit()
     return {'status': 'OK'}
 
 
 @router.post('/login')
 async def login_user(
+        db: DBDep,
         response: Response,
         data: UserRequestAdd = Body(openapi_examples={
     '1': {
@@ -57,21 +55,19 @@ async def login_user(
             'password': 'qwerty'
         },
     }})):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+    user = await db.users.get_user_with_hashed_password(email=data.email)
 
-        if not user or not AuthService().verify_password(data.password, user.hashed_password):
-            raise HTTPException(status_code=401, detail='Неверный email или пароль')
+    if not user or not AuthService().verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail='Неверный email или пароль')
 
-        access_token = AuthService().create_access_token({'user_id': user.id})
-        response.set_cookie('access_token', access_token)
-        return {'access_token': access_token}
+    access_token = AuthService().create_access_token({'user_id': user.id})
+    response.set_cookie('access_token', access_token)
+    return {'access_token': access_token}
 
 
 @router.get('/me')
-async def get_me(user_id: UserIdDep):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(id=user_id)
+async def get_me(user_id: UserIdDep, db: DBDep):
+    user = await db.users.get_one_or_none(id=user_id)
     return user
 
 
--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.exc import IntegrityError
 
-from src.database import async_session_maker
-from src.repositories.rooms import RoomsRepository
+from src.api.dependencies import DBDep
 from src.schemas.rooms import RoomAdd, RoomAddRequest, RoomPatchRequest, RoomPatch
 
 router = APIRouter(prefix='/hotels', tags=['Номера'])
@@ -11,69 +10,63 @@ router = APIRouter(prefix='/hotels', tags=['Номера'])
 @router.get('/{hotel_id}/rooms',
             summary='Получить список всех номеров отеля',
             description='Получить список всех номеров отеля')
-async def get_rooms(hotel_id: int):
-    async with async_session_maker() as session:
-        return await RoomsRepository(session).get_filtered(hotel_id=hotel_id)
+async def get_rooms(hotel_id: int, db: DBDep):
+        return await db.rooms.get_filtered(hotel_id=hotel_id)
 
 
 @router.get('/{hotel_id}/rooms/{room_id}',
             summary='Получить номер отеля по ID',
             description='Получить один номер отеля')
-async def get_room(hotel_id: int, room_id: int):
-    async with async_session_maker() as session:
-        room = await RoomsRepository(session).get_one_or_none(id=room_id, hotel_id=hotel_id)
-        if room is None:
-            raise HTTPException(status_code=404, detail='Номер не найден')
-        return room
+async def get_room(hotel_id: int, room_id: int, db: DBDep):
+    room = await db.rooms.get_one_or_none(id=room_id, hotel_id=hotel_id)
+    if room is None:
+        raise HTTPException(status_code=404, detail='Номер не найден')
+    return room
 
 
 @router.post('/{hotel_id}/rooms',
              summary='Добавить номер в отель',
              description='Создать и добавить новый номер в отель')
-async def create_room(hotel_id: int, room_data: RoomAddRequest):
+async def create_room(hotel_id: int, room_data: RoomAddRequest, db: DBDep):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
-    async with async_session_maker() as session:
-        try:
-            room = await RoomsRepository(session).add(_room_data)
-            await session.commit()
-        except IntegrityError:
-            raise HTTPException(status_code=404, detail='Отель не найден')
-        return {'status': 'OK', 'data': room}
+    try:
+        room = await db.rooms.add(_room_data)
+        await db.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail='Отель не найден')
+    return {'status': 'OK', 'data': room}
 
 
 @router.put('/{hotel_id}/rooms/{room_id}',
             summary='Полное обноление данных номера отеля',
             description='Обновить все параметры номера отеля')
-async def update_room(hotel_id: int, room_id: int, room_data: RoomAddRequest):
+async def update_room(hotel_id: int, room_id: int, room_data: RoomAddRequest, db: DBDep):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
-    async with async_session_maker() as session:
-        try:
-            await RoomsRepository(session).update(data=_room_data, id=room_id)
-            await session.commit()
-        except IntegrityError:
-            raise HTTPException(status_code=404, detail='Отель не найден')
-        return {'status': 'OK'}
+    try:
+        await db.rooms.update(data=_room_data, id=room_id)
+        await db.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail='Отель не найден')
+    return {'status': 'OK'}
 
 
 @router.patch('/{hotel_id}/rooms/{room_id}',
               summary='Частичное обноление данных номера отеля',
               description='Обновить один или несколько параметров номера отеля')
-async def edit_room_parameter(hotel_id: int, room_id: int, room_data: RoomPatchRequest):
+async def edit_room_parameter(hotel_id: int, room_id: int, room_data: RoomPatchRequest, db: DBDep):
     _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
-    async with async_session_maker() as session:
-        await RoomsRepository(session).update(data=_room_data, id=room_id, hotel_id=hotel_id, exclude_unset=True)
-        await session.commit()
-        return {'status': 'OK'}
+    await db.rooms.update(data=_room_data, id=room_id, hotel_id=hotel_id, exclude_unset=True)
+    await db.commit()
+    return {'status': 'OK'}
 
 
 @router.delete('/{hotel_id}/rooms/{room_id}',
                summary='Удалить номер отеля по ID',
                description='Удалить один номер отеля')
-async def delete_room(hotel_id: int, room_id: int):
-    async with async_session_maker() as session:
-        await RoomsRepository(session).delete(id=room_id, hotel_id=hotel_id)
-        await session.commit()
-        return {'status': 'OK'}
+async def delete_room(hotel_id: int, room_id: int, db: DBDep):
+    await db.rooms.delete(id=room_id, hotel_id=hotel_id)
+    await db.commit()
+    return {'status': 'OK'}
 
 
 
