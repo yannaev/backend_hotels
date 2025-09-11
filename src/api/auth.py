c6f9309f575f0254c4b59995755c0f9b4ce33159
Alexandr <79024450444@yandex.ru>
2025-09-11 20:00:51 +0500

    imports refactoring

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Body, HTTPException, Response
 
 from src.api.dependencies import UserIdDep, DBDep
-from src.database import async_session_maker
-from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.services.auth import AuthService
 
