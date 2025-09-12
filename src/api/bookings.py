6b041b11ad405b381b7a790b8fa101c81a9746e1
Alexandr <79024450444@yandex.ru>
2025-09-13 01:22:08 +0500

    create_booking post endpoint created

---
+++ src/api/bookings.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from src.api.dependencies import DBDep, UserIdDep
+from src.schemas.bookings import BookingAdd, BookingAddRequest
+
+router = APIRouter(prefix='/bookings', tags=['Бронирования'])
+
+
+@router.post('/')
+async def create_booking(user_id: UserIdDep, booking_data: BookingAddRequest, db: DBDep):
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    _booking_data = BookingAdd(user_id=user_id, price=room.price, **booking_data.model_dump())
+    await db.bookings.add(_booking_data)
+    await db.commit()
+    return {'status': 'OK'}
