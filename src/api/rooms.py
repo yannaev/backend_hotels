07cb7dd68ad3e799021b7cfe1bf94e9b0165499b
Alexandr <79024450444@yandex.ru>
2025-09-13 18:47:55 +0500

    get rooms by hotel_id and dates

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from datetime import date
+
+from fastapi import APIRouter, HTTPException, Query
 from sqlalchemy.exc import IntegrityError
 
 from src.api.dependencies import DBDep
@@ -10,8 +12,13 @@ router = APIRouter(prefix='/hotels', tags=['Номера'])
 @router.get('/{hotel_id}/rooms',
             summary='Получить список всех номеров отеля',
             description='Получить список всех номеров отеля')
-async def get_rooms(hotel_id: int, db: DBDep):
-        return await db.rooms.get_filtered(hotel_id=hotel_id)
+async def get_rooms(
+        hotel_id: int,
+        db: DBDep,
+        date_from: date = Query(example='2025-09-10'),
+        date_to: date = Query(example='2025-09-20')
+):
+        return await db.rooms.get_filtered_by_time(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
 
 
 @router.get('/{hotel_id}/rooms/{room_id}',
