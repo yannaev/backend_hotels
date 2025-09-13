31cf35a2bdea44a1636f4f5e70e7f1c7edd21d20
Alexandr <79024450444@yandex.ru>
2025-09-13 13:22:09 +0500

    create_booking post endpoint modified

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -6,10 +6,10 @@ from src.schemas.bookings import BookingAdd, BookingAddRequest
 router = APIRouter(prefix='/bookings', tags=['Бронирования'])
 
 
-@router.post('/')
+@router.post('')
 async def create_booking(user_id: UserIdDep, booking_data: BookingAddRequest, db: DBDep):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
     _booking_data = BookingAdd(user_id=user_id, price=room.price, **booking_data.model_dump())
-    await db.bookings.add(_booking_data)
+    booking = await db.bookings.add(_booking_data)
     await db.commit()
-    return {'status': 'OK'}
+    return {'status': 'OK', 'data': booking}
