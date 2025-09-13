3d3cd1fb4ccbdb2177af75190670c320321582c4
Alexandr <79024450444@yandex.ru>
2025-09-13 19:42:38 +0500

    get_all deleted

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,28 +13,6 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
-            self,
-            title,
-            location,
-            limit,
-            offset
-    ) -> list[Hotel]:
-        query = select(HotelsOrm)
-
-        if title:
-            query = query.filter(HotelsOrm.title.ilike(f'%{title.strip()}%'))
-
-        if location:
-            query = query.filter(HotelsOrm.location.ilike(f'%{location.strip()}%'))
-
-        query = (
-            query
-            .limit(limit)
-            .offset(offset)
-        )
-        result = await self.session.execute(query)
-        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
 
     async def get_filtered_by_time(
             self,
@@ -44,7 +22,7 @@ class HotelsRepository(BaseRepository):
             offset,
             date_from: date,
             date_to: date,
-    ):
+    ) -> list[Hotel]:
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
 
         hotels_ids_to_get = (
