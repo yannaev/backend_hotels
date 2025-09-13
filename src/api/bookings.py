4f859625f818d6265edf4e4827a75b8af3c84c5e
Alexandr <79024450444@yandex.ru>
2025-09-13 13:32:16 +0500

    GET bookings and GET my bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -6,6 +6,16 @@ from src.schemas.bookings import BookingAdd, BookingAddRequest
 router = APIRouter(prefix='/bookings', tags=['Бронирования'])
 
 
+@router.get('')
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get('/me')
+async def get_my_bookings(user_id: UserIdDep, db: DBDep):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
 @router.post('')
 async def create_booking(user_id: UserIdDep, booking_data: BookingAddRequest, db: DBDep):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
