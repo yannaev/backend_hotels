1e60b1543e2846c1ebc6c631bbe60c8b5d3049d5
Alexandr <79024450444@yandex.ru>
2025-10-01 00:17:25 +0500

    try test database, add pytest-asyncio

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,11 @@
+from src.database import async_session_maker
+from src.schemas.hotels import HotelAdd
+from src.utils.db_manager import DBManager
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Континенталь", location="Москва")
+
+    async with DBManager(session_factory=async_session_maker) as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f"{new_hotel_data=}")
