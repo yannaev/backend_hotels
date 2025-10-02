51a2cab08e7637409e3f2b54ec2be71ba37080ee
Alexandr <79024450444@yandex.ru>
2025-10-02 23:12:28 +0500

    add check_test_mode fixture

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.config import settings
+from src.database import Base, engine_null_pool
+from src.models import *
+
+
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode):
+    async with engine_null_pool.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -8,4 +8,5 @@ async def test_add_hotel():
 
     async with DBManager(session_factory=async_session_maker) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
+        await db.commit()
         print(f"{new_hotel_data=}")
