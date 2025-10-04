6c2034c2177a828de88b5c6183effa025d46017c
Alexandr <79024450444@yandex.ru>
2025-10-04 20:00:14 +0500

    add test_booking_crud

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -14,15 +14,31 @@ async def test_booking_crud(db):
         date_to=date(2024, 7, 10),
         price=100
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
     #read booking
-    await db.bookings.get_filtered(user_id=user_id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+
 
     #update booking
-    await db.bookings.update(booking_data)
+    update_booking_data = BookingAdd(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=date(2024, 7, 5),
+        date_to=date(2024, 7, 20),
+        price=500
+    )
+    await db.bookings.update(update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == date(2024, 7, 20)
+    assert updated_booking.price == 500
 
     #delete booking
-    await db.bookings.delete(user_id=user_id)
-
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
     await db.commit()
