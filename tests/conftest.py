5bbad923b0243e82ddad390f0d8d05cb0d89fc6e
Alexandr <79024450444@yandex.ru>
2025-10-04 22:16:52 +0500

    add authenticated_ac fixture for session-based authentication

--- tests/conftest.py
+++ tests/conftest.py
@@ -71,3 +71,19 @@ async def register_user(setup_database, ac):
             "password": "1234"
         }
     )
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def authenticated_ac(ac):
+    response = await ac.post(
+        "/auth/login",
+        json={
+            "email": "kot@pes.com",
+            "password": "1234"
+        }
+    )
+    assert response.status_code == 200
+    token = response.json().get("access_token")
+    assert token
+    ac.headers.update({"Authorization": f"Bearer {token}"})
+    return ac
