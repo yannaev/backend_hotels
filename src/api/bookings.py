7cae3dd375997b79e650bc28152700ef203e864b
Alexandr <79024450444@yandex.ru>
2025-10-05 15:00:20 +0500

    add_booking

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -19,7 +19,8 @@ async def get_my_bookings(user_id: UserIdDep, db: DBDep):
 @router.post('')
 async def create_booking(user_id: UserIdDep, booking_data: BookingAddRequest, db: DBDep):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    hotel = await db.hotels.get_one_or_none(id=room.hotel_id)
     _booking_data = BookingAdd(user_id=user_id, price=room.price, **booking_data.model_dump())
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
     await db.commit()
     return {'status': 'OK', 'data': booking}
--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,10 +1,13 @@
 from datetime import date
 
+from fastapi import HTTPException
 from sqlalchemy import select
 
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
+from src.repositories.rooms import RoomsRepository
+from src.repositories.utils import rooms_ids_for_booking
 
 
 class BookingsRepository(BaseRepository):
@@ -15,3 +18,14 @@ class BookingsRepository(BaseRepository):
         query = select(self.model).filter(self.model.date_from == date.today())
         result = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(model) for model in result.scalars().all()]
+
+    async def add_booking(self, booking_data, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(booking_data.date_from, booking_data.date_to, hotel_id)
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book = rooms_ids_to_book_res.scalars().all()
+
+        if booking_data.room_id in rooms_ids_to_book:
+            booking = await self.add(booking_data)
+            return booking
+        else:
+            raise HTTPException(status_code=409, detail='Нет свободных номеров на эти даты')
