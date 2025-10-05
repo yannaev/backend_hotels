a754a12e7db3da7a3221f2f85ac438ef654f4296
Alexandr <79024450444@yandex.ru>
2025-10-05 15:11:30 +0500

    add_booking

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -6,8 +6,8 @@ from sqlalchemy import select
 from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.mappers import BookingDataMapper
-from src.repositories.rooms import RoomsRepository
 from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingAdd
 
 
 class BookingsRepository(BaseRepository):
@@ -19,8 +19,11 @@ class BookingsRepository(BaseRepository):
         result = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(model) for model in result.scalars().all()]
 
-    async def add_booking(self, booking_data, hotel_id: int):
-        rooms_ids_to_get = rooms_ids_for_booking(booking_data.date_from, booking_data.date_to, hotel_id)
+    async def add_booking(self, booking_data: BookingAdd, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=booking_data.date_from,
+            date_to=booking_data.date_to,
+            hotel_id=hotel_id)
         rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
         rooms_ids_to_book = rooms_ids_to_book_res.scalars().all()
 
