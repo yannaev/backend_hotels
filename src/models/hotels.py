9356c87c5c4fe91845a908bc64e072caf9e3ed61
yannaev <79024450444@yandex.ru>
2025-10-25 21:05:13 +0500

    add cascade delete for hotel-rooms relationship and update ORM models

--- src/models/hotels.py
+++ src/models/hotels.py
@@ -1,8 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+import typing
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 
 from src.database import Base
 
+if typing.TYPE_CHECKING:
+    from src.models import RoomsOrm
 
 class HotelsOrm(Base):
     __tablename__ = "hotels"
@@ -10,3 +14,9 @@ class HotelsOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100), unique=True)
     location: Mapped[str] = mapped_column(String(100))
+
+    rooms: Mapped[list["RoomsOrm"]] = relationship(
+        back_populates="hotel",
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
--- src/models/rooms.py
+++ src/models/rooms.py
@@ -6,18 +6,20 @@ from sqlalchemy import ForeignKey
 from src.database import Base
 
 if typing.TYPE_CHECKING:
-    from src.models import FacilitiesOrm
+    from src.models import FacilitiesOrm, HotelsOrm
 
 
 class RoomsOrm(Base):
     __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id", ondelete="CASCADE"))
     title: Mapped[str]
     description: Mapped[str | None]
     price: Mapped[int]
     quantity: Mapped[int]
+
+    hotel: Mapped[list["HotelsOrm"]] = relationship(back_populates="rooms")
 
     facilities: Mapped[list["FacilitiesOrm"]] = relationship(
         secondary="rooms_facilities",
