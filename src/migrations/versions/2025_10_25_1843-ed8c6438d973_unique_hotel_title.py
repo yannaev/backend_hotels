d3ee327d299c58a795300ba1507b3ce49c82003e
yannaev <79024450444@yandex.ru>
2025-10-25 19:27:55 +0500

    unique hotel title migration

---
+++ src/migrations/versions/2025_10_25_1843-ed8c6438d973_unique_hotel_title.py
@@ -0,0 +1,29 @@
+"""unique hotel title
+
+Revision ID: ed8c6438d973
+Revises: fda376c885e5
+Create Date: 2025-10-25 18:43:50.270084
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "ed8c6438d973"
+down_revision: Union[str, Sequence[str], None] = "fda376c885e5"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_unique_constraint(None, "hotels", ["title"])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint(None, "hotels", type_="unique")
