8f394fafcc27e5d19ce99b17028a3110023a7b04
yannaev <79024450444@yandex.ru>
2025-10-25 21:04:56 +0500

    add PostgreSQL indexing convention and metadata to database model

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
 
 from src.config import settings
 
@@ -12,5 +13,16 @@ async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 async_session_maker_null_pull = async_sessionmaker(bind=engine_null_pool, expire_on_commit=False)
 
 
+POSTGRES_INDEXES_NAMING_CONVENTION = {
+    "ix": "%(column_0_label)s_idx",
+    "uq": "%(table_name)s_%(column_0_name)s_key",
+    "ck": "%(table_name)s_%(constraint_name)s_check",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey",
+}
+
+metadata = MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION)
+
+
 class Base(DeclarativeBase):
-    pass
+    metadata = metadata
